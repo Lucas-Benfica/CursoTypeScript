686dcde1d03743184f514063a47597db8e23906a
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-09-11 22:11:29 -0300

    Ex3

--- ex1/src/index.ts
+++ ex1/src/index.ts
@@ -1,6 +1,22 @@
 //Exercícios 1 e 2
-function sum(num1: number, num2: number){
+function sum(num1: number, num2: number) :number {
     return num1 + num2;
+}
+
+function multiply(num1: number, num2: number) :number {
+    return num1 * num2;
+}
+
+function isEven(num:number) :boolean {
+    return num % 2 === 0;
+}
+
+function showResult(result :number) :void {
+    if(isEven(result)) {
+        console.log(`The result is ${result} and it's even!`);
+    } else {
+        console.log(`The result is ${result} and it's even!`);
+    }
 }
 
 const result = sum(3, 2);
